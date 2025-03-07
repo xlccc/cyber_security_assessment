ef98696e8bc0bfa46b7387278aee7560aa07d50e
attente12 <2382498766@qq.com>
2025-03-07 14:02:53 +0800

    1

--- ServerManager.cpp
+++ ServerManager.cpp
@@ -7,7 +7,7 @@ using namespace concurrency::streams;
 
 ServerManager::ServerManager()
     : localConfig{
-        "10.9.130.61",  // host
+        "10.9.130.37",  // host
         33060,            // port
         "root",           // user
         "Navicat822!", // password
@@ -16,7 +16,7 @@ ServerManager::ServerManager()
     pool(localConfig),    // 使用 localConfig 初始化 pool
     dbManager(DB_PATH)    // 原有的 dbManager 初始化
 {
-    utility::string_t address = _XPLATSTR("http://10.9.130.61:8081/");
+    utility::string_t address = _XPLATSTR("http://10.9.130.37:8081/");
     uri_builder uri(address);
     auto addr = uri.to_uri().to_string();
     listener = std::make_unique<http_listener>(addr);
@@ -172,8 +172,8 @@ void ServerManager::handle_request(http_request request) {
 
 void ServerManager::redis_get_scan(http_request request) {
     
-    std::cout << check_redis_unauthorized("root","12341234","12341234","10.9.130.61") << std::endl;
-    std::cout << check_pgsql_unauthorized("root", "12341234","postgres","12341234" ,"10.9.130.61","5432" ) << std::endl;
+    std::cout << check_redis_unauthorized("root","12341234","12341234","10.9.130.37") << std::endl;
+    std::cout << check_pgsql_unauthorized("root", "12341234","postgres","12341234" ,"10.9.130.37","5432" ) << std::endl;
     request.reply(web::http::status_codes::OK, "result");
 }
 
