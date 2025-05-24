9ba8f7ec5c9155d0695d4e312178d928147fd1c0
俊杰 黄 <3488979767@qq.com>
2025-05-24 16:08:04 +0800

    修改bug

--- database/DatabaseHandler.cpp
+++ database/DatabaseHandler.cpp
@@ -2243,7 +2243,7 @@ BaselineCheckSummary DatabaseHandler::calculateBaselineSummary(const std::vector
     }
 
     // 计算不合规项数
-    summary.non_compliant_items = summary.total_checks - summary.compliant_items;
+    summary.non_compliant_items = summary.total_checks - summary.compliant_items - summary.half_compliant_items;
 
 
 
